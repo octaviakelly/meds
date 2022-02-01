84407e06026ae865b2e05912e2d1e0791a8d533c
octaviakelly <35118034+octaviakelly@users.noreply.github.com>
2022-02-01 14:16:47 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import sqlite3 as sqlite
 
+import matplotlib.pyplot as plt
+
 def create_database():
     os.chdir(os.path.dirname(__file__))
     curPath = os.getcwd()
@@ -24,7 +26,38 @@ def create_database():
             print("Heart rate: ", row[6])
             print("Dose: ", row[7])
             print("\n")
+        heartRate = dbConnCur.execute("SELECT Heart_rate FROM meds")
+        global heartRateTup
+        heartRateTup=()
+        for item in heartRate:
+            heartRateTup+=item
+        dose = dbConnCur.execute("SELECT dose FROM meds")
+        global doseTup
+        doseTup=()
+        for item in dose:
+            doseTup+=item
+        systolic = dbConnCur.execute("SELECT Systolic_BP FROM meds")
+        global systolicTup
+        systolicTup = ()
+        for item in systolic:
+            systolicTup += item
+
         dbConnCur.close()
 
-
 create_database()
+
+plt.subplot(2,1,1)
+plt.plot(doseTup, heartRateTup)
+plt.xlabel("Dose (mg)")
+plt.ylabel("Heart rate (bpm)")
+plt.subplot(2,1,2)
+plt.plot(doseTup, systolicTup)
+plt.xlabel("Dose (mg)")
+plt.ylabel("Systolic bp")
+plt.show()
+plt.boxplot(systolicTup)
+plt.title("Looking for outlying systolic bp measurements")
+plt.ylabel("Systolic bp")
+plt.show()
+
+
